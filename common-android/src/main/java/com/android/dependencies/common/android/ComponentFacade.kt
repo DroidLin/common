ab36cc8f10c0b1b899b9d0d7d74af6e8026dd195
liuzhongao <1047771038@qq.com>
2024-06-24 22:10:19 +0800

    modify initial of context.

--- common-android/src/main/java/com/android/dependencies/common/android/ComponentFacade.kt
+++ common-android/src/main/java/com/android/dependencies/common/android/ComponentFacade.kt
@@ -8,6 +8,6 @@ import com.android.dependencies.common.ComponentFacade
  * @since 2024/4/12 20:45
  */
 
-fun ComponentFacade.initContext(context: Context) {
-    (this[IContext::class.java] as? ContextImpl)?.attachApplicationContext(context)
+fun installContext(context: Context) {
+    ContextImpl.attachApplicationContext(context)
 }
